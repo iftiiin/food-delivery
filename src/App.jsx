1c8bca573fefc6ddbd6f3638517921dd40117707
iftiiin <iftinawil9@gmail.com>
2025-04-14 23:57:15 +0300

    feat: Define main application routes for Home, Order, About, Contact, and Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import { Route, Routes } from 'react-router'
+import Home from './pages/Home'
+import Order from './pages/Order'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Dashboard from './pages/Dashboard'
 const App = () => {
   return (
     <>
       <Header/>
       <main>
-        
+        {/* routes */}
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/order' element={<Order />}/>
+            <Route  path='/about' element={<About />}/>
+            <Route  path='/contact' element={<Contact />}/>
+            <Route  path='/dashboard' element={<Dashboard />}/>
+        </Routes>
       </main>
       <Footer/>
     </>
