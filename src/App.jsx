73a72126f29cbb928eeea8d7d670b4a1d142aa80
iftiiin <iftinawil9@gmail.com>
2025-04-17 00:42:12 +0300

    refactor: Add UnAuthenticatedRoute and ProtectedRoute wrappers to existing routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,44 @@ import SignUp from './pages/SignUp'
 import { Toaster } from 'react-hot-toast'
 import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
+import { AuthProvider } from './context/AuthContext'
+import UnAuthenticatedRoute from './context/UnAuthenticatedRoute'
+import ProtectedRoute from './context/ProtectedRoute'
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <Header/>
       <main>
         {/* routes */}
         <Routes>
             <Route path='/' element={<Home/>} />
             <Route path='/order' element={<Order />}/>
-            <Route  path='/about' element={<About />}/>
-            <Route  path='/contact' element={<Contact />}/>
-            <Route  path='/dashboard' element={<Dashboard />}/>
-            <Route  path='/signup' element={<SignUp />}/>
-            <Route  path='/signin' element={<SignIn />}/>
-            <Route  path='/profile' element={<Profile />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/contact' element={<Contact />}/>
+            {/* unauthenticated routes */}
+            <Route path='/signup' element={
+              <UnAuthenticatedRoute>
+                  <SignUp />
+              </UnAuthenticatedRoute>
+              }/>
+            <Route path='/signin' element={
+              <UnAuthenticatedRoute>
+                  <SignIn />
+              </UnAuthenticatedRoute>
+              }/>
+            <Route path='/profile' element={<Profile />}/>
+            
+            {/* protected route */}
+            <Route path='/dashboard' element={
+              <ProtectedRoute>
+                  <Dashboard />
+              </ProtectedRoute>
+              }/>
         </Routes>
       </main>
       <Footer/>
       <Toaster />
-    </>
+    </AuthProvider>
   )
 }
 
